b652ee655561937012d95d90e509bf6a386946cc
Guilherme <guilherme94siqueira@gmail.com>
2024-06-04 21:35:14 -0300

    finalizando CRUD EventoDAO

--- Backend/index.php
+++ Backend/index.php
@@ -1,10 +1,11 @@
 <?php
 require_once "config/Database.php";
-require_once "dao/ReservaDAO.php";
+require_once "dao/EventoDAO.php";
+require_once "entity/Evento.php";
 
-$reservaDAO = new ReservaDAO();
-$reserva = new Reserva(null, "Reservado", "2024-03-10", "2024-05-20", "08:00:56", "12:30:34", "2-3-4-6", 1, 1 );
-echo $reservaDAO->delete(2);
+$eventoDAO = new EventoDAO();
+$evento = new Evento(2, "Técnico em jardinagem", "Aecio", "295689");
+echo $eventoDAO->delete(2);
 
 ?>
 
