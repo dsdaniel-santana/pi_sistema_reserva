edcdc0ada5907d13ec66137eb68c4dc03761cbb3
daniel_santana_86 <daniel_santana_87@hotmail.com>
2024-06-13 19:40:09 -0300

    criação dos arquivos eventos.php e eventos_add.php

---
+++ eventos.php
@@ -0,0 +1,63 @@
+<?php
+require_once "Backend/config/Database.php";
+require_once "Backend/dao/ReservaDAO.php";
+require_once "Backend/dao/SalaDAO.php";
+
+
+$reservaDAO = new ReservaDAO();
+$reservas = $reservaDAO->getAll();
+
+$salaDAO = new SalaDAO();
+$salas = $salaDAO->getAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Lista de Eventos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1 class="my-4">Lista de Eventos</h1>
+        <a href="add_reserva.php" class="btn btn-primary mb-4">Adicionar Contato</a>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <?php foreach ($reservas as $reserva) : ?>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Status: <?php echo $reserva->getStatus_sala(); ?></h5>
+                            <p class="card-text">Data Inicio: <?php echo htmlspecialchars($reserva->getData_inicio(), ENT_QUOTES, 'UTF-8'); ?></p>
+                            <p class="card-text">Data fim: <?php echo htmlspecialchars($reserva->getData_fim(), ENT_QUOTES, 'UTF-8'); ?></p>
+                            <p class="card-text">Horario inicio: <?php echo htmlspecialchars($reserva->getHorario_inicio(), ENT_QUOTES, 'UTF-8'); ?></p>
+                            <p class="card-text">Horario fim: <?php echo htmlspecialchars($reserva->getHoraio_fim(), ENT_QUOTES, 'UTF-8'); ?></p>
+                            <p class="card-text">Dias da semana: <?php echo htmlspecialchars($reserva->getDias_semana(), ENT_QUOTES, 'UTF-8'); ?></p>
+                            <a href="add_reserva.php?id=<?php echo $reserva->getId(); ?>" class="btn btn-primary">Detalhes</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ eventos_add.php
@@ -0,0 +1,132 @@
+<?php
+
+//session_start(); // Inicia uma sessão na página
+
+// if(!isset($_SESSION['token'])) {
+//     header('Location: auth.php');
+//     exit();
+// }
+
+require_once 'Backend/dao/ReservaDAO.php';
+require_once 'Backend/entity/Reserva.php';
+
+$reservaDAO = new ReservaDAO();
+$reserva = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
+    $reserva  = $reservaDAO->getById($_GET['id']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['save'])) {
+        if (isset($_POST['id']) && !empty($_POST['id'])) {
+            $reserva  = $reservaDAO->getById($_POST['id']);
+            $dias_semanaStr = implode(", ", $_POST['dias']);
+
+            $reserva->setStatus_sala($_POST['status_sala']);
+            $reserva->setData_inicio($_POST['data_inicio']);
+            $reserva->setData_fim($_POST['data_fim']);
+            $reserva->setHorario_inicio($_POST['horario_inicio']);
+            $reserva->setHoraio_fim($_POST['horario_fim']);
+            $reserva->setDias_semana($dias_semanaStr);
+            $reserva->setEvento_id($_POST['evento_id']);
+            $reserva->setSala_id($_POST['sala_id']);
+
+            $reservaDAO->update($reserva);
+        } else {
+            $novaReserva = new Reserva(null, $_POST['status_sala'], $_POST['data_inicio'], $_POST['data_fim'], $_POST['horario_inicio'], $_POST['horario_fim'], $_POST['dias_semana'], 1, 1);
+            $reservaDAO->create($novaReserva);
+        }
+
+        header('Location: index.php');
+        exit;
+    }
+
+    if (isset($_POST['delete']) && isset($_POST['id'])) {
+        $reservaDAO->delete($_POST['id']);
+        header('Location: index.php');
+        exit;
+    }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Evento</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+    <div class="container">
+        <h1 class="my-4">Detalhes do Evento</h1>
+        <form action="add_reserva.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $reserva ? $reserva->getId() : ''  ?>">
+            <div class="card">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="status_sala">status:</label>
+                        <input type="text" class="form-control" id="status_sala" name="status_sala" value="<?php echo $reserva ? $reserva->getStatus_sala() : ''  ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="data_inicio">Data Inicio:</label>
+                        <input type="date" class="form-control" id="data_inicio" name="data_inicio" value="<?php echo $reserva ? $reserva->getData_inicio() : ''  ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="data_fim">Data Fim:</label>
+                        <input type="datetime" class="form-control" id="data_fim" name="data_fim" value="<?php echo $reserva ? $reserva->getData_fim() : ''  ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="horario_inicio">horario Inicio:</label>
+                        <input type="time" class="form-control" id="horario_inicio" name="horario_inicio" value="<?php echo $reserva ? $reserva->getHorario_inicio() : ''  ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="horario_fim">Horario Fim:</label>
+                        <input type="time" class="form-control" id="horario_fim" name="horario_fim" value="<?php echo $reserva ? $reserva->getHoraio_fim() : ''  ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <h5>Dias Da Semana:</h5>
+                        <div style="display: flex;">
+                            <label for="seg">Seg:</label>
+                            <input type="checkbox" class="form-control" id="seg" name="dias[]" value="1">
+                            <label for="ter">Ter:</label>
+                            <input type="checkbox" class="form-control" id="ter" name="dias[]" value="2">
+                            <label for="qua">Qua:</label>
+                            <input type="checkbox" class="form-control" id="qua" name="dias[]" value="3">
+                            <label for="qui">Qui:</label>
+                            <input type="checkbox" class="form-control" id="qui" name="dias[]" value="4">
+                            <label for="sex">Sex:</label>
+                            <input type="checkbox" class="form-control" id="sex" name="dias[]" value="5">
+                            <label for="sab">Sab:</label>
+                            <input type="checkbox" class="form-control" id="sab" name="dias[]" value="6">
+                            <label for="dom">Dom:</label>
+                            <input type="checkbox" class="form-control" id="dom" name="dias[]" value="1">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="evento_id">Evento id:</label>
+                        <input type="number" class="form-control" id="evento_id" name="evento_id" value="<?php echo $reserva ? $reserva->getEvento_id() : ''  ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="sala_id">Sala id:</label>
+                        <input type="number" class="form-control" id="sala_id" name="sala_id" value="<?php echo $reserva ? $reserva->getSala_id() : ''  ?>" required>
+                    </div>
+                    <button type="submit" name="save" class="btn btn-success">Salvar</button>
+                    <?php if ($reserva) : ?>
+                        <button type="submit" name="delete" class="btn btn-danger">Excluir</button>
+                    <?php endif ?>
+                    <a href="index.php" class="btn btn-secondary">Voltar</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
