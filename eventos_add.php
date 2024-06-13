7e57a59d90b08565dfdbffc546b6e28d3e86d395
daniel_santana_86 <daniel_santana_87@hotmail.com>
2024-06-13 19:56:06 -0300

    atualizando

--- eventos_add.php
+++ eventos_add.php
@@ -72,52 +72,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="card">
                 <div class="card-body">
                     <div class="form-group">
-                        <label for="status_sala">status:</label>
+                        <label for="status_sala">Titulo do Evento:</label>
+                        <!-- <label for="status_sala">status:</label> -->
                         <input type="text" class="form-control" id="status_sala" name="status_sala" value="<?php echo $reserva ? $reserva->getStatus_sala() : ''  ?>" required>
                     </div>
+
                     <div class="form-group">
-                        <label for="data_inicio">Data Inicio:</label>
-                        <input type="date" class="form-control" id="data_inicio" name="data_inicio" value="<?php echo $reserva ? $reserva->getData_inicio() : ''  ?>">
+                        <label for="status_sala">Docente:</label>
+                        <!-- <label for="status_sala">status:</label> -->
+                        <input type="text" class="form-control" id="status_sala" name="status_sala" value="<?php echo $reserva ? $reserva->getStatus_sala() : ''  ?>" required>
                     </div>
+
                     <div class="form-group">
-                        <label for="data_fim">Data Fim:</label>
-                        <input type="datetime" class="form-control" id="data_fim" name="data_fim" value="<?php echo $reserva ? $reserva->getData_fim() : ''  ?>" required>
+                        <label for="status_sala">Oferta:</label>
+                        <!-- <label for="status_sala">status:</label> -->
+                        <input type="text" class="form-control" id="status_sala" name="status_sala" value="<?php echo $reserva ? $reserva->getStatus_sala() : ''  ?>" required>
                     </div>
-                    <div class="form-group">
-                        <label for="horario_inicio">horario Inicio:</label>
-                        <input type="time" class="form-control" id="horario_inicio" name="horario_inicio" value="<?php echo $reserva ? $reserva->getHorario_inicio() : ''  ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="horario_fim">Horario Fim:</label>
-                        <input type="time" class="form-control" id="horario_fim" name="horario_fim" value="<?php echo $reserva ? $reserva->getHoraio_fim() : ''  ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <h5>Dias Da Semana:</h5>
-                        <div style="display: flex;">
-                            <label for="seg">Seg:</label>
-                            <input type="checkbox" class="form-control" id="seg" name="dias[]" value="1">
-                            <label for="ter">Ter:</label>
-                            <input type="checkbox" class="form-control" id="ter" name="dias[]" value="2">
-                            <label for="qua">Qua:</label>
-                            <input type="checkbox" class="form-control" id="qua" name="dias[]" value="3">
-                            <label for="qui">Qui:</label>
-                            <input type="checkbox" class="form-control" id="qui" name="dias[]" value="4">
-                            <label for="sex">Sex:</label>
-                            <input type="checkbox" class="form-control" id="sex" name="dias[]" value="5">
-                            <label for="sab">Sab:</label>
-                            <input type="checkbox" class="form-control" id="sab" name="dias[]" value="6">
-                            <label for="dom">Dom:</label>
-                            <input type="checkbox" class="form-control" id="dom" name="dias[]" value="1">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="evento_id">Evento id:</label>
-                        <input type="number" class="form-control" id="evento_id" name="evento_id" value="<?php echo $reserva ? $reserva->getEvento_id() : ''  ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="sala_id">Sala id:</label>
-                        <input type="number" class="form-control" id="sala_id" name="sala_id" value="<?php echo $reserva ? $reserva->getSala_id() : ''  ?>" required>
-                    </div>
+
+
+
                     <button type="submit" name="save" class="btn btn-success">Salvar</button>
                     <?php if ($reserva) : ?>
                         <button type="submit" name="delete" class="btn btn-danger">Excluir</button>
